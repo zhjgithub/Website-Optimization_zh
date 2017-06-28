9d0a5e10ca0bfff81ac87a5c3aebb5a9d96a731f
organism <neuqzhj@live.com>
2017-06-28 11:26:54 +0800

    feature: Use gulp minify and replace path

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
 const gulp = require('gulp'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
     imageResize = require('gulp-image-resize'),
     imagemin = require('gulp-imagemin'),
     imageminJpegoptim = require('imagemin-jpegoptim'),
@@ -7,40 +10,49 @@ const gulp = require('gulp'),
     fs = require('fs'),
     htmlmin = require('gulp-htmlmin'),
     rename = require("gulp-rename"),
+    // notify = require('gulp-notify'),
     del = require('del');
 
-gulp.task('images-responsive', () =>
-    gulp.src('src/views/images/pizzeria.jpg', {
-        base: 'src'
-    })
-    .pipe(imageResize({
-        width: 100,
-        filter: 'Catrom'
-    }))
-    .pipe(imagemin({
-        progressive: true
-    }))
-    .pipe(rename(path => path.basename += "-small"))
-    .pipe(gulp.dest('dist'))
-    .pipe(imageResize({
-        width: 360,
-        filter: 'Catrom'
-    }))
-    .pipe(imagemin({
-        progressive: true
-    }))
-    .pipe(rename(path => path.basename += "-middle"))
-    .pipe(gulp.dest('dist'))
-    .pipe(imageResize({
-        width: 720,
-        filter: 'Catrom'
-    }))
-    .pipe(imagemin({
-        progressive: true
-    }))
-    .pipe(rename(path => path.basename += "-large"))
-    .pipe(gulp.dest('dist'))
-);
+function minifyHtml(stream) {
+    return stream.pipe(replace(/(href="css\/[^."]+)(.css)/g, '$1.min$2'))
+        .pipe(replace(/(src="js\/[^."]+)(.js)/g, '$1.min$2'))
+        .pipe(htmlmin({
+            minifyCSS: true,
+            minifyJS: {
+                mangle: false,
+                compress: false
+            },
+            removeComments: true
+        }))
+        .pipe(replace(/(\s*\n){3,}/g, '\n\n'))
+        .pipe(gulp.dest('dist/'))
+}
+
+let resizeImageTasks = [];
+
+[100, 360, 720].forEach(function (size) {
+    let resizeImageTask = 'resize_' + size;
+    gulp.task(resizeImageTask, function () {
+        return gulp.src('src/views/images/pizzeria.jpg', {
+                base: 'src'
+            })
+            .pipe(imageResize({
+                width: size,
+                filter: 'Catrom'
+            }))
+            .pipe(imagemin({
+                progressive: true,
+                verbose: true
+            }))
+            .pipe(rename({
+                suffix: '-' + size
+            }))
+            .pipe(gulp.dest('dist'))
+    });
+    resizeImageTasks.push(resizeImageTask);
+});
+
+gulp.task('resize-images', resizeImageTasks);
 
 gulp.task('images', () =>
     gulp.src(['src/img/*.{png,jpg}', 'src/views/images/*.{png,jpg}', '!src/views/images/pizzeria.jpg'], {
@@ -51,48 +63,67 @@ gulp.task('images', () =>
             max: 85,
             progressive: true
         }),
+        imagemin.optipng()
     ], {
-        interlaced: true,
-        progressive: true,
-        optimizationLevel: 5,
         verbose: true
     }))
     .pipe(gulp.dest('dist'))
-)
-
-gulp.task('html', () =>
-    gulp.src('src/index.html')
-    .pipe(replace(/<link href="css\/style.css"[^>]*>/, function (s) {
-        var style = fs.readFileSync('src/css/style.css', 'utf8');
-        return '<style>\n' + style + '\n</style>';
-    }))
-    .pipe(replace(/(views\/images\/pizzeria)(.jpg)/, '$1-small$2'))
-    .pipe(inlineImages({
-        selector: 'img[src]'
-    }))
-    .pipe(htmlmin({
-        minifyCSS: true,
-        minifyJS: {
-            mangle: false,
-            compress: false
-        },
-        removeComments: true
-    }))
-    .pipe(replace(/(\s*\r\n){3,}/g, '\r\n\r\n'))
-    .pipe(gulp.dest('dist/'))
 );
 
-gulp.task('copy', () =>
-    gulp.src(['src/**', '!src/index.html', '!src/img/*', '!src/views/images/*'], {
+gulp.task('styles', () =>
+    gulp.src(['src/css/*.css', 'src/views/css/*.css'], {
         base: 'src'
     })
+    .pipe(minifycss())
+    .pipe(rename({
+        suffix: '.min'
+    }))
     .pipe(gulp.dest('dist'))
-)
+    // .pipe(notify({
+    //     message: 'Styles task complete'
+    // }))
+);
+
+gulp.task('scripts', () =>
+    gulp.src(['src/js/*.js', 'src/views/js/*.js'], {
+        base: 'src'
+    })
+    .pipe(jshint('.jshintrc'))
+    // .pipe(jshint.reporter('default'))
+    // .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(rename({
+        suffix: '.min'
+    }))
+    .pipe(gulp.dest('dist'))
+    // .pipe(notify({
+    //     message: 'Scripts task complete'
+    // }))
+);
+
+gulp.task('index', function () {
+    let stream = gulp.src('src/index.html')
+        .pipe(replace(/<link href="css\/style.css"[^>]*>/, function (s) {
+            let style = fs.readFileSync('src/css/style.css', 'utf8');
+            return '<style>\n' + style + '\n</style>';
+        }));
+
+    return minifyHtml(stream);
+});
+
+gulp.task('replace-min-files', function () {
+    let stream = gulp.src(['src/*.html', 'src/views/*.html', '!src/index.html'], {
+        base: 'src'
+    });
+
+    return minifyHtml(stream);
+});
+
 
 gulp.task('clean', function (cb) {
-    del(['dist/*']).then(cb())
-})
+    del(['dist/**']).then(cb())
+});
 
-gulp.task('default', ['clean'], function () {
-    gulp.start('copy', 'html', 'images-responsive', 'images');
+gulp.task('default', function () {
+    gulp.start('index', 'replace-min-files', 'images', 'resize-images', 'styles', 'scripts');
 });
